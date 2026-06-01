b0a77dfcfdd6043e210b7bbe1c050a68c5041811
ikad770 <ikad770@gmail.com>
2026-06-01 09:24:34 +0300

    Delete src directory

--- src/components/KpiCard.jsx
+++
@@ -1,9 +0,0 @@
-export default function KpiCard({ label, value, detail }) {
-  return (
-    <section className="kpi-card">
-      <span>{label}</span>
-      <strong>{value}</strong>
-      {detail && <small>{detail}</small>}
-    </section>
-  );
-}
